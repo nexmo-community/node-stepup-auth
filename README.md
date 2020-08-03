72f39e7814745ecc51a49fd8d95f25d833cdf261
Mark Lewin <mark.lewin@vonage.com>
2020-08-03 15:47:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Using the Verify API for Step-up Authentication
+
+This repo contains the example code for our [tutorial]() on using the Nexmo Verify API to authenticate a user by their mobile phone number. It is written in Node.js using the Express framework.
+
+## Installing your own version
+Follow these steps to get your own version of this up and running:
+
+```bash
+git clone https://github.com/nexmo-community/node-stepup-auth.git
+cd node-stepup-auth && npm install
+```
+
+## Configuring the application
+Once installed, copy the `.env-example` file to `.env` in the application's root directory. Enter your API key and secret from the [Nexmo Developer Dashboard](https://dashboard.nexmo.com) and also a name for your application which will appear on the home page and also in the `from` field of any SMS sent via the Verify API.
+
+```
+NEXMO_API_KEY=YOUR NEXMO API KEY
+NEXMO_API_SECRET=YOUR NEXMO API SECRET
+NEXMO_BRAND_NAME=UP TO 11 ALPHANUMERIC CHARACTERS
+```
+## Running the application
+You should then be able to run the app with `npm start`. Open `http://localhost:3000` in your browser to begin.
---
+++ server.js
@@ -0,0 +1,119 @@
+require('dotenv').load();
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const app = express();
+const Nexmo = require('nexmo');
+
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const NEXMO_BRAND_NAME = process.env.NEXMO_BRAND_NAME;
+
+const nexmo = new Nexmo(
+	{
+		apiKey: NEXMO_API_KEY,
+		apiSecret: NEXMO_API_SECRET,
+	},
+	{
+		debug: true,
+	}
+);
+
+let verifyRequestId = null;
+let verifyRequestNumber = null;
+
+app.use(express.static('public'));
+
+app.use(
+	session({
+		secret: 'loadsofrandomstuff',
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	/*
+    If there is a session for the user, the `index.html`
+    page will display the number that was used to authenticate them.
+    If not, it will prompt them to authenticate.
+  */
+	if (!req.session.user) {
+		res.render('index', {
+			brand: NEXMO_BRAND_NAME,
+		});
+	} else {
+		res.render('index', {
+			number: req.session.user.number,
+			brand: NEXMO_BRAND_NAME,
+		});
+	}
+});
+
+app.get('/authenticate', (req, res) => {
+	res.render('authenticate');
+});
+
+app.post('/verify', (req, res) => {
+	// Start the verification process
+	verifyRequestNumber = req.body.number;
+	nexmo.verify.request(
+		{
+			number: verifyRequestNumber,
+			brand: NEXMO_BRAND_NAME,
+		},
+		(err, result) => {
+			if (err) {
+				console.error(err);
+			} else {
+				verifyRequestId = result.request_id;
+				console.log(`request_id: ${verifyRequestId}`);
+			}
+		}
+	);
+	/* 
+    Redirect to page where the user can 
+    enter the code that they received
+  */
+	res.render('entercode');
+});
+
+app.post('/check-code', (req, res) => {
+	// Check the code provided by the user
+	nexmo.verify.check(
+		{
+			request_id: verifyRequestId,
+			code: req.body.code,
+		},
+		(err, result) => {
+			if (err) {
+				console.error(err);
+			} else {
+				if (result.status == 0) {
+					/* User provided correct code,
+             so create a session for that user
+          */
+					req.session.user = {
+						number: verifyRequestNumber,
+					};
+				}
+			}
+			// Redirect to the home page
+			res.redirect('/');
+		}
+	);
+});
+
+app.get('/cancel', (req, res) => {
+	req.session.destroy();
+	res.redirect('/');
+});
+
+const server = app.listen(3000, () => {
+	console.log(`Server running on port ${server.address().port}`);
+});
